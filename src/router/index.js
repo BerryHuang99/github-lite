9ee7c3503a149cd73ba0e0070aef46bb13df8619
Berry Huang <berryhuang99@gmail.com>
2017-11-23 23:30:28 +0800

    the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/components/pages/HomePage'
+import Repositories from '@/components/pages/Repositories'
+import Stars from '@/components/pages/Stars'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: HomePage
+    },
+    {
+      path: '/repositories',
+      name: 'Repositories',
+      component: Repositories
+    },
+    {
+      path: '/stars',
+      name: 'Stars',
+      component: Stars
     }
   ]
 })
