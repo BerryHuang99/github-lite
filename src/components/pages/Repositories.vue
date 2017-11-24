6949411499baf7391eb394b304dc3b5668232025
Berry Huang <berryhuang99@gmail.com>
2017-11-24 16:57:52 +0800

    the repositories page

--- src/components/pages/Repositories.vue
+++ src/components/pages/Repositories.vue
@@ -1,7 +1,7 @@
 <template>
     <el-row>
-        <el-col :span="24">
-            <repository></repository>
+        <el-col :span="24" v-for="item in repositories">
+            <repository :item="item"></repository>
         </el-col>
     </el-row>
 </template>
@@ -10,6 +10,17 @@
 import Repository from "../Repository";
 
 export default {
+    name: 'Repositories',
+    data() {
+        return {
+            repositories: [{
+                name: 'name',
+                description: 'Personal experiments from Northeastern University.',
+                language: 'html',
+                update: 'Up date 30 days ago',
+            }],
+        }
+    },
     components: {
         Repository
     }
